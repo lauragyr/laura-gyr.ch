8eaf50c938783b51a59422536297db16390e60ee
lauragyr <laura.gyr@hotmail.com>
2019-01-09 22:52:08 +0100

    animated all the menus, added intro-animation-tests (metaballs)

--- js/menu-animation.js
+++ js/menu-animation.js
@@ -1,8 +1,28 @@
-function switchOnClick()
+//intro-menubar
+function switchViolet()
 {
   let menubar = document.getElementById("menubar");
   menubar.id = "menubarActive";
   setTimeout(function functionName() {
     window.location.href = "about.html";
+  }, 1000);
+}
+
+//cv-menubar
+function switchWhite()
+{
+  let menubar = document.getElementById("menubar");
+  menubar.id = "menubarActive";
+  setTimeout(function functionName() {
+    window.location.href = "index.html";
+  }, 1000);
+}
+
+function switchCircle()
+{
+  let menubar = document.getElementById("menucircle");
+  menucircle.id = "menucircleActive";
+  setTimeout(function functionName() {
+    window.location.href = "index.html";
   }, 1000);
 }
